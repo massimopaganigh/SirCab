68278bf17eb67a32a4222ad019de8ab5e7d463be
mpagani <Massimo.Pagani@unitec-group.com>
2025-07-08 14:06:46 +0200

    enhance: rimuovere librerie esterne non utilizzate, creation time

--- src/UniCab/Program.cs
+++ src/UniCab/Program.cs
@@ -2,16 +2,22 @@
 {
     internal class Program
     {
-        private static void Process_OutputDataReceived(object sender, DataReceivedEventArgs e) => Log.Information(e.Data ?? string.Empty);
+        private static void Process_OutputDataReceived(object sender, DataReceivedEventArgs e) => Log.Information($"(makecab.exe) {e.Data}" ?? string.Empty);
 
-        private static void Process_ErrorDataReceived(object sender, DataReceivedEventArgs e) => Log.Error(e.Data ?? string.Empty);
+        private static void Process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Log.Error($"(makecab.exe) {e.Data}" ?? string.Empty);
+
+            Environment.ExitCode = 1;
+        }
 
         public static void Main(string[] args)
         {
             try
             {
                 Version? version = Assembly.GetExecutingAssembly().GetName().Version;
-                string? framework = AppDomain.CurrentDomain.SetupInformation.TargetFrameworkName;
+                string? targetFrameworkName = AppDomain.CurrentDomain.SetupInformation.TargetFrameworkName;
+                DateTime creationTime = File.GetCreationTime(AppContext.BaseDirectory);
                 OperatingSystem oSVersion = Environment.OSVersion;
 
                 Console.WriteLine($@"
@@ -20,8 +26,8 @@
 ███    ███ ███   ███ ███▌ ███    █▀    ███    ███   ███    ███
 ███    ███ ███   ███ ███▌ ███          ███    ███  ▄███▄▄▄██▀
 ███    ███ ███   ███ ███▌ ███        ▀███████████ ▀▀███▀▀▀██▄
-███    ███ ███   ███ ███  ███    █▄    ███    ███   ███    ██▄
-███    ███ ███   ███ ███  ███    ███   ███    ███   ███    ███ {version} - {framework}
+███    ███ ███   ███ ███  ███    █▄    ███    ███   ███    ██▄ {version} - {targetFrameworkName}
+███    ███ ███   ███ ███  ███    ███   ███    ███   ███    ███ {creationTime}
 ████████▀   ▀█   █▀  █▀   ████████▀    ███    █▀  ▄█████████▀ {oSVersion}
 ");
                 Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
@@ -34,12 +40,16 @@
                 {
                     Log.Error("Ddf file path is null or empty.");
 
+                    Environment.ExitCode = 1;
+
                     return;
                 }
 
                 if (!File.Exists(ddfFilePath))
                 {
                     Log.Error("Ddf file does not exist.");
+
+                    Environment.ExitCode = 1;
 
                     return;
                 }
@@ -69,6 +79,8 @@
             catch (Exception ex)
             {
                 Log.Error(ex, nameof(Main));
+
+                Environment.ExitCode = 1;
             }
             finally
             {
