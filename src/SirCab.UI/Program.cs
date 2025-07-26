0805cbc3fdb3906b5208fb34b6a4ec12207f099e
ギャップ <mp120805a@gmail.com>
2025-07-26 18:01:21 +0200

    fix: build

--- src/SirCab.UI/Program.cs
+++ src/SirCab.UI/Program.cs
@@ -3,7 +3,7 @@
     internal sealed class Program
     {
         public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>().UsePlatformDetect().WithInterFont().LogToTrace();
-        
+
         [STAThread]
         public static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
     }
