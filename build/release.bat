7549553a701db0aa502a20606b4d36a5f5b32a7b
ギャップ <mp120805a@gmail.com>
2025-08-28 08:43:45 +0200

    fix: winget release

--- build/release.bat
+++ build/release.bat
@@ -7,12 +7,21 @@ set "wingetcreate_path=..\src\ext\Microsoft.WindowsPackageManagerManifestCreator
 
 for /f "usebackq delims=" %%i in ("..\VERSION") do set "version_raw=%%i"
 
-echo Releasing SirCab.SirCab...
+echo Releasing SirCab.SirCabCLI...
 
-%wingetcreate_path% update --submit --token "%sybau%" --urls "https://github.com/massimopaganigh/SirCab/releases/download/%version_raw%/SirCab.CLI_fat.zip" --version %version_raw% SirCab.SirCab
+%wingetcreate_path% update --submit --token "%sybau%" --urls "https://github.com/massimopaganigh/SirCab/releases/download/%version_raw%/SirCab.CLI_fat.zip" --version %version_raw% SirCab.SirCabCLI
 
 if %ERRORLEVEL% neq 0 (
-    echo Release of SirCab.SirCab failed.
+    echo Release of SirCab.SirCabCLI failed.
+    exit /b %ERRORLEVEL%
+)
+
+echo Releasing SirCab.SirCabUI...
+
+%wingetcreate_path% update --submit --token "%sybau%" --urls "https://github.com/massimopaganigh/SirCab/releases/download/%version_raw%/SirCab.UI_fat.zip" --version %version_raw% SirCab.SirCabUI
+
+if %ERRORLEVEL% neq 0 (
+    echo Release of SirCab.SirCabUI failed.
     exit /b %ERRORLEVEL%
 )
 
